07e76d6b4917cec879a3c0cbd730228ed8203026
Dancan sikuku <dancansikuku94@gmail.com>
2021-06-08 14:02:59 +0300

    another commit

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,106 @@
 <template>
-  <div
-    id="app"
-    :class="
-      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
-    "
-  >
-    <main>
-      <div>
-        <div
-          class="
-            h-4
-            w-9
-            text-black
-            font-sans
-            bg-white
-            justify-items-center
-            pt-6
-            ml-5
-          "
-        >
-          <input
-            type="text"
-            class="search-bar"
-            placeholder="Search"
-            v-model="query"
-            @keypress="fetchWeather"
-          />
+  <div class="w-104 h-220 pt-4">
+    <div
+      id="app"
+      :class="
+        typeof weather.main != 'undefined' && weather.main.temp_max > 16
+          ? 'warm'
+          : ''
+      "
+    >
+      <main>
+        <div>
+          <div
+            class="
+              h-4
+              w-9
+              text-black
+              font-sans
+              bg-white
+              justify-items-center
+              pt-3
+              ml-1
+            "
+          >
+            <input
+              type="text"
+              class="search-bar"
+              placeholder="Search"
+              v-model="query"
+              @keypress="fetchWeather"
+            />
+          </div>
         </div>
-      </div>
-      <div class="pt-10">
-        <img src="/map.jpg" alt="" />
-      </div>
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="bg-white pt-2">
-          <h1 class="text-black text-xl text-bold pt-4 ml-5 font-ubuntu">
-            {{ weather.name }}, {{ weather.sys.country }}
-          </h1>
-          <h2 class="text-black-500 text-xs ml-5 font-ubuntu">
-            Weather forecast.
-          </h2>
-          <h3 class="text-black-500 text-sm ml-5 font-ubuntu">
-            Today, {{ dateBuilder() }}
-          </h3>
+        <div class="pt-10">
+          <img src="/map.jpg" alt="" />
         </div>
-        <div class="flex flex-row bg-white w-10">
-          <div class="bg-white pt-4 rounded-md">
-            <h1 class="text-black font-medium justify-items-center ml-10">
-              Temperature.
+        <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+          <div class="bg-white pt-2 ml-0">
+            <h1 class="text-black text-xl text-bold pt-4 ml-1 font-ubuntu">
+              {{ weather.name }}, {{ weather.sys.country }}
             </h1>
-            <div class="pt-6 h-5 w-4 flex-auto flex space-x-3">
-              <div class="flex-auto flex-items-center">
-                <h2 class="text-blue-600 font-bold">
-                  {{ Math.round(weather.main.temp_max) }}°c
-                </h2>
-                <p class="text-black font-normal">Maximum</p>
+            <h2 class="text-black-500 text-xs ml-1 font-ubuntu">
+              Weather forecast.
+            </h2>
+            <h3 class="text-black-500 text-sm ml-1 font-ubuntu">
+              Today, {{ dateBuilder() }}
+            </h3>
+          </div>
+          <div class="flex flex-row bg-white w-100">
+            <div class="bg-white pt-4 rounded-md">
+              <h1 class="text-black font-roboto justify-items-center ml-10">
+                Temperature
+              </h1>
+              <div class="pt-6 h-5 w-4 flex-auto flex space-x-3">
+                <div class="flex-auto flex-items-center">
+                  <h2 class="text-blue-600 font-bold">
+                    {{ Math.round(weather.main.temp_max) }}°c
+                  </h2>
+                  <p class="text-black font-normal">Maximum</p>
+                </div>
+                <div class="flex-auto flex-items-center">
+                  <h3 class="text-blue-600 font-bold">
+                    {{ Math.round(weather.main.temp_min) }}°c
+                  </h3>
+                  <p class="text-black font-normal">Minimum</p>
+                </div>
               </div>
-              <div class="flex-auto flex-items-center">
-                <h3 class="text-blue-600 font-bold">
-                  {{ Math.round(weather.main.temp_min) }}°c
-                </h3>
-                <p class="text-black font-normal">Minimum</p>
+            </div>
+            <div class="font-medium ml-10 pt-4 rounded-md">
+              <h1 class="text-black font-roboto">Additional Parameters</h1>
+              <div class="flex flex-col">
+                <div class="text-blue font-normal">
+                  Wind {{ Math.round(weather.main.wind_deg) }}
+                </div>
+                <div class="text-blue font-normal">
+                  pressure {{ Math.round(weather.main.pressure) }}Pa
+                </div>
+                <div class="text-blue font-normal">
+                  Precipitation{{ Math.round(weather.main.precipitation) }}
+                </div>
               </div>
             </div>
           </div>
-          <div class="font-medium ml-10 pt-4 rounded-md">
-            <h1 class="text-black font-roboto align-center">
-              Additional Parameters
-            </h1>
-            <p class="text-blue font-normal">
-              Wind {{ Math.round(weather.main.wind_deg) }}
-            </p>
-            <p class="text-blue font-normal">
-              pressure {{ Math.round(weather.main.pressure) }}Pa
-            </p>
-            <p class="text-blue font-normal">
-              Precipitation{{ Math.round(weather.main.precipitation) }}
-            </p>
+          <div class="flex flex-row bg-gray-50 pt-4 h-4 w-10">
+            <div class="text-black">
+              <p class="text-black font-normal">Tuesday</p>
+              <img src="/sun.png" alt="" class="w=4/12 h=1/60" />
+              <p class="text-black font-light">{{ weather.main.icons }}</p>
+            </div>
+            <div class="text-black ml-10">
+              <p class="text-black font-normal">Wednesday</p>
+              <img src="/cloud.png" alt="" class="w-4/12 h-6/12" />
+              <p class="text-black font-light">Sunny Interval.</p>
+            </div>
+            <div class="text-black ml-10">
+              <p class="text-black font-normal">Thursday</p>
+              <img src="/rain.jpg" alt="" class="w-4/12 h-6/12" />
+              <p class="text-black font-light">Heavy Rain.</p>
+            </div>
           </div>
         </div>
-        <div class="flex flex-row bg-gray-50 pt-4 h-4 w-10">
-          <div class="text-black">
-            <p class="text-black font-normal">Tuesday</p>
-            <img src="/sun.png" alt="" class="w=4/12 h=1/60" />
-            <p class="text-black font-light">{{ weather.main.icons }}</p>
-          </div>
-          <div class="text-black ml-10">
-            <p class="text-black font-normal">Wednesday</p>
-            <img src="/cloud.png" alt="" class="w-4/12 h-6/12" />
-            <p class="text-black font-light">Sunny Interval.</p>
-          </div>
-          <div class="text-black ml-10">
-            <p class="text-black font-normal">Thursday</p>
-            <img src="/rain.jpg" alt="" class="w-4/12 h-6/12" />
-            <p class="text-black font-light">Heavy Rain.</p>
-          </div>
-        </div>
-      </div>
-    </main>
+      </main>
+    </div>
   </div>
 </template>
 

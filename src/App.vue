28f19f69bf75140b57e71ca5fd4a16e323691060
Dancan sikuku <dancansikuku94@gmail.com>
2021-06-23 11:42:56 +0300

    changes in the repo

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
               {{ weather.name }}, {{ weather.sys.country }}
             </h1>
             <h2 class="text-black-500 text-xs ml-1 font-ubuntu">
-              Weather forecast for Today
+              Weather forecast.
             </h2>
             <h3 class="text-black-500 text-sm ml-1 font-ubuntu">
               Today, {{ dateBuilder() }}
